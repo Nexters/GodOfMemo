5221375f2660dcd4acb2e58304faffcb5457b165
lifenjoy51 <lifenjoy51@hotmail.com>
2014-01-29 20:38:12 +0900

    opengl es 2.0 basic setting

--- GodOfMemo/src/com/nexters/godofmemo/MainActivity.java
+++ GodOfMemo/src/com/nexters/godofmemo/MainActivity.java
@@ -1,5 +1,10 @@
 package com.nexters.godofmemo;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -7,13 +12,74 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.nexters.godofmemo.render.FirstOpenGLProjectRenderer;
+
 public class MainActivity extends ActionBarActivity {
+	/**
+	 * Hold a reference to our GLSurfaceView
+	 */
+	private GLSurfaceView glSurfaceView;
+	private boolean rendererSet = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		glSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager =
+            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+ 
+        final ConfigurationInfo configurationInfo =
+            activityManager.getDeviceConfigurationInfo();
+        /*
+		
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+         */
+        // Even though the latest emulator supports OpenGL ES 2.0,
+        // it has a bug where it doesn't set the reqGlEsVersion so
+        // the above check doesn't work. The below will detect if the
+        // app is running on an emulator, and assume that it supports
+        // OpenGL ES 2.0.
+        final boolean supportsEs2 =
+            configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                 && (Build.FINGERPRINT.startsWith("generic")
+                  || Build.FINGERPRINT.startsWith("unknown")
+                  || Build.MODEL.contains("google_sdk")
+                  || Build.MODEL.contains("Emulator")
+                  || Build.MODEL.contains("Android SDK built for x86")));
+
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            // Assign our renderer.
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            rendererSet = true;
+        } else {
+            /*
+             * This is where you could create an OpenGL ES 1.x compatible
+             * renderer if you wanted to support both ES 1 and ES 2. Since we're
+             * not doing anything, the app will crash if the device doesn't
+             * support OpenGL ES 2.0. If we publish on the market, we should
+             * also add the following to AndroidManifest.xml:
+             * 
+             * <uses-feature android:glEsVersion="0x00020000"
+             * android:required="true" />
+             * 
+             * This hides our app from those devices which don't support OpenGL
+             * ES 2.0.
+             */
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+
 	}
 
 	@Override
@@ -29,10 +95,12 @@ public class MainActivity extends ActionBarActivity {
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 		case R.id.action_search:
-			Toast.makeText(getApplicationContext(), "search", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), "search",
+					Toast.LENGTH_SHORT).show();
 			return true;
 		case R.id.action_settings:
-			Toast.makeText(getApplicationContext(), "setting", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), "setting",
+					Toast.LENGTH_SHORT).show();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
---
+++ GodOfMemo/src/com/nexters/godofmemo/render/FirstOpenGLProjectRenderer.java
@@ -0,0 +1,47 @@
+package com.nexters.godofmemo.render;
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class FirstOpenGLProjectRenderer implements Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        // Set the background clear color to red. The first component is
+        // red, the second is green, the third is blue, and the last
+        // component is alpha, which we don't use in this lesson.
+        glClearColor(0.3f, 0.6f, 0.9f, 0.0f);
+    }
+
+    /**
+     * onSurfaceChanged is called whenever the surface has changed. This is
+     * called at least once when the surface is initialized. Keep in mind that
+     * Android normally restarts an Activity on rotation, and in that case, the
+     * renderer will be destroyed and a new one created.
+     * 
+     * @param width
+     *            The new width, in pixels.
+     * @param height
+     *            The new height, in pixels.
+     */
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        // Set the OpenGL viewport to fill the entire surface.
+        glViewport(0, 0, width, height);
+    }
+
+    /**
+     * OnDrawFrame is called whenever a new frame needs to be drawn. Normally,
+     * this is done at the refresh rate of the screen.
+     */
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        // Clear the rendering surface.
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
