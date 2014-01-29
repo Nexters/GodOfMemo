8c0645966d3df481468b3e9922d46bd848c72829
lifenjoy51 <lifenjoy51@hotmail.com>
2014-01-29 21:08:36 +0900

    renderer test

--- GodOfMemo/src/com/nexters/godofmemo/MainActivity.java
+++ GodOfMemo/src/com/nexters/godofmemo/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.nexters.godofmemo.render.MemoRenderer;
+import com.nexters.godofmemo.render.AirHockeyRenderer;
 import com.nexters.godofmemo.view.MemoGLView;
 
 public class MainActivity extends ActionBarActivity {
@@ -26,81 +26,81 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
+
 		glSurfaceView = new MemoGLView(this);
 
-        // Check if the system supports OpenGL ES 2.0.
-        final ActivityManager activityManager =
-            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
- 
-        final ConfigurationInfo configurationInfo =
-            activityManager.getDeviceConfigurationInfo();
-        
-        /*
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-         */
-        
-        // Even though the latest emulator supports OpenGL ES 2.0,
-        // it has a bug where it doesn't set the reqGlEsVersion so
-        // the above check doesn't work. The below will detect if the
-        // app is running on an emulator, and assume that it supports
-        // OpenGL ES 2.0.
-        final boolean supportsEs2 =
-            configurationInfo.reqGlEsVersion >= 0x20000
-                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                 && (Build.FINGERPRINT.startsWith("generic")
-                  || Build.FINGERPRINT.startsWith("unknown")
-                  || Build.MODEL.contains("google_sdk")
-                  || Build.MODEL.contains("Emulator")
-                  || Build.MODEL.contains("Android SDK built for x86")));
+		// Check if the system supports OpenGL ES 2.0.
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 
-        if (supportsEs2) {
-            // Request an OpenGL ES 2.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(2);
+		final ConfigurationInfo configurationInfo = activityManager
+				.getDeviceConfigurationInfo();
 
-            // Assign our renderer.
-            glSurfaceView.setRenderer(new MemoRenderer());
-            rendererSet = true;
-        } else {
-            /*
-             * This is where you could create an OpenGL ES 1.x compatible
-             * renderer if you wanted to support both ES 1 and ES 2. Since we're
-             * not doing anything, the app will crash if the device doesn't
-             * support OpenGL ES 2.0. If we publish on the market, we should
-             * also add the following to AndroidManifest.xml:
-             * 
-             * <uses-feature android:glEsVersion="0x00020000"
-             * android:required="true" />
-             * 
-             * This hides our app from those devices which don't support OpenGL
-             * ES 2.0.
-             */
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
-                Toast.LENGTH_LONG).show();
-            return;
-        }
+		/*
+		 * final boolean supportsEs2 = configurationInfo.reqGlEsVersion >=
+		 * 0x20000;
+		 */
 
-        setContentView(glSurfaceView);
+		// Even though the latest emulator supports OpenGL ES 2.0,
+		// it has a bug where it doesn't set the reqGlEsVersion so
+		// the above check doesn't work. The below will detect if the
+		// app is running on an emulator, and assume that it supports
+		// OpenGL ES 2.0.
+		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000
+				|| (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 && (Build.FINGERPRINT
+						.startsWith("generic")
+						|| Build.FINGERPRINT.startsWith("unknown")
+						|| Build.MODEL.contains("google_sdk")
+						|| Build.MODEL.contains("Emulator") || Build.MODEL
+							.contains("Android SDK built for x86")));
+
+		if (supportsEs2) {
+			// Request an OpenGL ES 2.0 compatible context.
+			glSurfaceView.setEGLContextClientVersion(2);
+
+			// Assign our renderer.
+			//glSurfaceView.setRenderer(new MemoRenderer());
+			glSurfaceView.setRenderer(new AirHockeyRenderer(getApplicationContext()));
+			rendererSet = true;
+		} else {
+			/*
+			 * This is where you could create an OpenGL ES 1.x compatible
+			 * renderer if you wanted to support both ES 1 and ES 2. Since we're
+			 * not doing anything, the app will crash if the device doesn't
+			 * support OpenGL ES 2.0. If we publish on the market, we should
+			 * also add the following to AndroidManifest.xml:
+			 * 
+			 * <uses-feature android:glEsVersion="0x00020000"
+			 * android:required="true" />
+			 * 
+			 * This hides our app from those devices which don't support OpenGL
+			 * ES 2.0.
+			 */
+			Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+
+		setContentView(glSurfaceView);
 
 	}
-	
-	@Override
-    protected void onPause() {
-        super.onPause();
-        
-        if (rendererSet) {
-            glSurfaceView.onPause();
-        }
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        
-        if (rendererSet) {
-            glSurfaceView.onResume();
-        }
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (rendererSet) {
+			glSurfaceView.onPause();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (rendererSet) {
+			glSurfaceView.onResume();
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ GodOfMemo/src/com/nexters/godofmemo/util/LoggerConfig.java
@@ -0,0 +1,5 @@
+package com.nexters.godofmemo.util;
+
+public class LoggerConfig {
+	public static final boolean ON = true;
+}
