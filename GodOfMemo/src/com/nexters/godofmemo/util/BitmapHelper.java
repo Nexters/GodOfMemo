a45d297b7ea918e15514b96c45625efad5b58989
lifenjoy51 <lifenjoy51@hotmail.com>
2014-02-19 17:18:05 +0900

    dinamically arrange text in memo. by maxlength and maxlines

--- GodOfMemo/src/com/nexters/godofmemo/util/BitmapHelper.java
+++ GodOfMemo/src/com/nexters/godofmemo/util/BitmapHelper.java
@@ -1,6 +1,8 @@
 package com.nexters.godofmemo.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
@@ -15,6 +17,11 @@ import android.graphics.Rect;
 public class BitmapHelper {
 	
 	private static Map<Integer, Bitmap> bitmapCache = new HashMap<Integer, Bitmap>();
+	
+	//메모 한줄 최대 길이
+	private static final int maxLength = 10;	//TODO 최대길이는 임시값.
+	//메모 최대 줄 개수
+	private static final int maxLine= 3;	//TODO 최대 줄 개수는 임시값.
 
 	/**
 	 * 이미지에 텍스트를 쓰는 함수
@@ -117,10 +124,13 @@ public class BitmapHelper {
 	 * @return
 	 */
 	public static Bitmap drawTextToBitmap(String gText) {
+		
+		int width = 400;
+		int height = 300;
 
 		// Read in the resource
-		Bitmap bitmap = Bitmap.createBitmap(400, 300, Bitmap.Config.ARGB_8888);
-
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		// Canvas
 		Canvas canvas = new Canvas(bitmap);
 		// new antialised Paint
 		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -130,26 +140,69 @@ public class BitmapHelper {
 		int textSize = (int) (32);
 		paint.setTextSize(textSize);
 		// text shadow
-		paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+		//paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
 		
-
-
-		//텍스트를 자른다.
-		int maxLength = 5;
-		if(gText.length()>maxLength){
-			gText = gText.substring(0, maxLength);
-			gText += "...";
-		}
+		//##########
+		//텍스트 여러줄 처리
+		//##########
+		String dividedText = getDividedText(gText);
+		
+		//텍스트를 줄바꿈 단위로 쪼갠다.
+		String[] dividedTextArray = dividedText.split("\n");
 
 		// draw text to the Canvas center
-		Rect bounds = new Rect();
-		paint.getTextBounds(gText, 0, gText.length(), bounds);
-		int x = (bitmap.getWidth() - bounds.width()) / 2;
-		int y = (bitmap.getHeight() + bounds.height()) / 2;
-		
-		// TODO 텍스트를 메모 위 어느 위치에 그릴것인지 정해야 한다.
-		canvas.drawText(gText, x, y, paint);
+		int x = width / 2;
+		int y = height / 2;
 
+		int loopCnt = 0;
+		int textOffsetY = 0;
+		int margin = 3;
+		int offset = (textSize + margin)/1;
+		
+		//몇번 포문을 수행할지 결정
+		if(dividedTextArray.length < maxLine){
+			loopCnt = dividedTextArray.length;
+		}else{
+			loopCnt = maxLine;
+		}
+		
+		//시작 높이 위치 정하기
+		textOffsetY = y - (offset/2)*(loopCnt-1) + margin;
+		
+		//여러줄 출력하기
+		for(int i=0; i<loopCnt; i++){
+			String text = dividedTextArray[i];
+			int px = x - (text.length() * textSize)/2;
+			int py = textOffsetY + (i*offset);
+			canvas.drawText(text, px, py, paint);
+		}
+		
 		return bitmap;
+	}
+	
+	/**
+	 * 텍스트를 특정 길이마다 행변환시킨다.
+	 * @param text
+	 * @return
+	 */
+	private static String getDividedText(String text){
+		StringBuffer sb = new StringBuffer();
+		int cnt = 0;
+		for(int i=0; i<text.length(); i++){
+			char c = text.charAt(i);
+			
+			if(cnt == maxLength){
+				cnt = 1;
+				sb.append('\n');
+			}else if(c == '\n'){
+				cnt = 0;
+			}else{
+				cnt++;
+			}
+			
+			sb.append(c);
+		}
+		String dividedText = sb.toString();
+		return dividedText;
 	}
 }
