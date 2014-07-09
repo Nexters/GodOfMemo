9d2ba7f88f802dfc50fe1cbf2b03777a07c7bf9f
subin <subin.c.park@gmail.com>
2014-07-10 03:03:23 +0900

    튜토리얼 꺼지는 버그 고침

--- GodOfMemo/src/com/nexters/godofmemo/TutorialActivity.java
+++ GodOfMemo/src/com/nexters/godofmemo/TutorialActivity.java
@@ -4,102 +4,35 @@ import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
-import com.nexters.godofmemo.frg.FirstTutorialFragment;
-import com.nexters.godofmemo.frg.FourthTutorialFragment;
-import com.nexters.godofmemo.frg.SecondTutorialFragment;
-import com.nexters.godofmemo.frg.ThirdTutorialFragment;
+import com.nexters.godofmemo.adapter.FullScreenImageAdapter;
 import com.nexters.godofmemo.util.Font;
 
+@SuppressLint("NewApi")
 public class TutorialActivity extends FragmentActivity {
-	  /**
-     * The number of pages (wizard steps) to show in this demo.
-     */
-    private static final int NUM_PAGES = 5;
+	private FullScreenImageAdapter adapter;
+	private ViewPager viewPager;
+	//private ArrayList<String> images;
 
-    /**
-     * The pager widget, which handles animation and allows swiping horizontally to access previous
-     * and next wizard steps.
-     */
-    private ViewPager mPager;
-
-    /**
-     * The pager adapter, which provides the pages to the view pager widget.
-     */
-    private PagerAdapter mPagerAdapter;
-
-    @SuppressLint("NewApi")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tutorial);
 
-		//폰트 초기화
+		// 폰트 초기화
 		Font.setTf(Typeface.createFromAsset(getAssets(), "fonts/nanum.ttf"));
-				
+		
 		//커스톰 액션바 구현.
 		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); 
 		getActionBar().setCustomView(R.layout.actionbar_tutorial);
+		
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		
+		adapter = new FullScreenImageAdapter(TutorialActivity.this);
 
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
-    }
+		viewPager.setAdapter(adapter);
+	}
 
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
-
-    /**
-     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-     * sequence.
-     */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-    		switch (position) {
-
-    		case 0:
-    			return new FirstTutorialFragment();
-    			
-    		case 1:
-    			return new SecondTutorialFragment();
-    			
-    		case 2:
-    			return new ThirdTutorialFragment();
-    			
-    		case 3:
-    			return new FourthTutorialFragment();
-
-    		default:
-    			return new FourthTutorialFragment();
-
-    		}
-            
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
 }
---
+++ GodOfMemo/src/com/nexters/godofmemo/adapter/FullScreenImageAdapter.java
@@ -0,0 +1,82 @@
+package com.nexters.godofmemo.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.nexters.godofmemo.R;
+
+public class FullScreenImageAdapter extends PagerAdapter {
+	private int[] mImages;
+	
+	
+	private Activity activity;
+    private LayoutInflater inflater;
+ 
+    // constructor
+    public FullScreenImageAdapter(Activity tempActivity) {
+        this.activity = tempActivity;
+        
+        mImages = new int[] {
+                R.drawable.tutorial1,
+                R.drawable.tutorial2,
+                R.drawable.tutorial3,
+                R.drawable.tutorial4,
+                R.drawable.tutorial5,
+            };
+    }
+ 
+    @Override
+    public int getCount() {
+        return mImages.length;
+    }
+ 
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((RelativeLayout) object);
+    }
+     
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imgDisplay;
+        Button btnClose;
+  
+        inflater = (LayoutInflater) activity
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container,
+                false);
+  
+        imgDisplay = (ImageView) viewLayout.findViewById(R.id.imgDisplay);
+        btnClose = (Button) viewLayout.findViewById(R.id.btnClose);
+        
+        imgDisplay.setImageResource(mImages[position]);
+        
+        
+         
+        // close button click event
+        btnClose.setOnClickListener(new View.OnClickListener() {            
+            @Override
+            public void onClick(View v) {
+                activity.finish();
+            }
+        });
+  
+        ((ViewPager) container).addView(viewLayout);
+  
+        return viewLayout;
+    }
+     
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((RelativeLayout) object);
+  
+    }
+
+}
