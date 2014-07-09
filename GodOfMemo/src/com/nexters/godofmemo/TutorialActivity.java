43189fedde98d5969dc830b30d16d831880bf4a2
subin <subin.c.park@gmail.com>
2014-07-10 03:13:58 +0900

    튜토리얼에 액션바 없애고 프래그먼트 정리

--- GodOfMemo/src/com/nexters/godofmemo/TutorialActivity.java
+++ GodOfMemo/src/com/nexters/godofmemo/TutorialActivity.java
@@ -1,14 +1,11 @@
 package com.nexters.godofmemo;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
 import com.nexters.godofmemo.adapter.FullScreenImageAdapter;
-import com.nexters.godofmemo.util.Font;
 
 @SuppressLint("NewApi")
 public class TutorialActivity extends FragmentActivity {
@@ -19,14 +16,10 @@ public class TutorialActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	    getActionBar().hide();
+	    
 		setContentView(R.layout.activity_tutorial);
 
-		// 폰트 초기화
-		Font.setTf(Typeface.createFromAsset(getAssets(), "fonts/nanum.ttf"));
-		
-		//커스톰 액션바 구현.
-		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); 
-		getActionBar().setCustomView(R.layout.actionbar_tutorial);
 		
 		viewPager = (ViewPager) findViewById(R.id.pager);
 		
